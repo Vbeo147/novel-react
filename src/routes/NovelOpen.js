2e44795f73898eb035b6e5527b17db0d033b54f7
Vbeo147 <vbeo147@gmail.com>
2022-08-14 15:39:53 +0900

    Updated Open

--- src/routes/NovelOpen.js
+++ src/routes/NovelOpen.js
@@ -9,11 +9,13 @@ function NovelOpen() {
   const result = Local.findIndex((Info) => parseInt(Info.id) === parseInt(id));
   return (
     <div className={style.open_header}>
-      <div className={style.open_header__main}>
+      <div className={style.open_header__title}>
         <div>{Local[result].title}</div>
+      </div>
+      <div className={style.open_header__text}>
         <div>{Local[result].text}</div>
       </div>
-      <div>
+      <div className={style.open_header__btn}>
         <button
           onClick={() => {
             navigate("/");
